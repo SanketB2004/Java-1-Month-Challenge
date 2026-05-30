c034e581ec88b2215621db058c7fda1f8e0dac32
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-05-30 10:07:35 +0530

    Added Java learning materials

---
+++ OOPS CWS/Encapsulation/exampleno1.java
@@ -0,0 +1,29 @@
+import mypackage.MyClass;
+class Employee{
+
+    int empid = 2 ;
+
+    int basicsalary = 10000;
+    int incentives = 5000;
+
+    void countsalary(){
+        int totalsalary = basicsalary + incentives;
+        System.out.println("Total Salary is : " + totalsalary);
+    }
+
+}
+
+
+
+
+
+
+public class exampleno1 {
+
+    public static void main(String[] args) {
+        
+//Employee emp = new Employee();
+
+a.print();
+    }
+}
