99e04d421c850e83511bf95544f4c871e843b4f5
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-06-13 12:26:37 +0530

    NO

---
+++ Engineering D/statickeyword/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+
+    private  int id;
+
+    private String name;
+
+public static int count = 0;
+
+    public int age;
+
+// this is class instence
+
+    public static void main(String[] args) {
+        
+
+
+    }
+}
