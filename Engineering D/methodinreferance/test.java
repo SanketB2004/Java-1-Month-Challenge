d7d8cfc5a2797dfd800fd84d38e0d75910a79ae1
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-06-13 11:24:25 +0530

    Done

--- Engineering D/methodinreferance/test.java
+++ Engineering D/methodinreferance/test.java
@@ -17,7 +17,7 @@ public class test {
     
     
     
-        //     System.out.println(sum(1,2,3,4,5));
+        //     System.out.println(sum(1,2));
     // }
     // public static int sum(int... a){
 
