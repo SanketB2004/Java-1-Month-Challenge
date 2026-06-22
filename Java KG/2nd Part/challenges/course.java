a4c198a6a589309a93ccd9299b6016fc3a4102be
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-06-22 11:11:33 +0530

    Ok

--- Java KG/2nd Part/challenges/course.java
+++ Java KG/2nd Part/challenges/course.java
@@ -1,15 +1,20 @@
 package challenges;
 
 class course {
+    String[] enrolledStudents;
+
     static int maxcapacity = 30;
 int enrollment;
     String courseName;
     String enrroledStudent;
-course(String courseName) {
+
+     
+    course(String courseName) {
         this.courseName = courseName;
         this.enrollment = 0;
+        this.enrolledStudents = new String[maxcapacity];
     }
-    String[] enrolledStudents=new String[maxcapacity];
+    //String[] enrolledStudents=new String[maxcapacity];
     
     static void setMaxCapacity(int capacity) {
         course.maxcapacity = capacity;
